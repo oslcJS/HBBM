59d5a3ae9f2e29f18ebf1e6b0c657bad0caf17e6
PDX122 <wiredflux@gmail.com>
2026-05-09 08:33:43 -0600

    Initial commit: HBBM PaperMC plugin v1.0 (lowkey will never update this)

---
+++ src/main/java/com/hbbm/HBBM.java
@@ -0,0 +1,19 @@
+package com.hbbm;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HBBM extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(null);
+    }
+}
